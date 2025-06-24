f30b21bca3ab707d4eed94239b247339e63d92db
sachin-limaja-cognizant <141751020+sachin-limaja-cognizant@users.noreply.github.com>
2025-06-24 11:10:00 +0530

    Update config.js

--- tools/sidekick/config.js
+++ tools/sidekick/config.js
@@ -1,6 +1,5 @@
 {
   "project": "My Project",
   "editUrlLabel": "Document Authoring",
-  "parser": "@typescript-eslint/parser",
-  "editUrlPattern": "https://da.live/edit#/sachin-limaja-cognizant/adobe-optimizer-demo/"
+  "editUrlPattern": "https://da.live/edit\#/sachin-limaja-cognizant/adobe-optimizer-demo"
 }
